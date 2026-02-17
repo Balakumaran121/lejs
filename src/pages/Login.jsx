a3d7ba3a8a9a2cdf16fe16c822f6b8298bc27362
Balakumaran <bkumaran121@gmail.com>
2026-02-17 21:59:50 +0530

    Updated Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className="rounded bg-gray-50 border border-gray-100">
+      <h1>Login</h1>
+      <label htmlFor="Email">Email</label>
+      <input
+        type="email"
+        value="email"
+        placeholder="Enter youe email"
+        name="email"
+      />
+      <label htmlFor="Password">Password</label>
+      <input type="password" placeholder="***" value={""} />
     </div>
-  )
+  );
 }
 
 export default Login
